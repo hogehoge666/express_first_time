b730c739683c4368a8d0d9049774ee0e4821e682
tnomura <hogehoge666@gmail.com>
2021-01-30 23:34:33 +0900

    Revert "server returns index.html"
    This reverts commit 06aa3457b43ac0cd4fd0a4ef29fbc821bbc8bbb1.

--- serve.js
+++ serve.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/public/index.html`);
+    res.send('Hello World!');
     console.log('/へアクセスがありました');
 });
 
